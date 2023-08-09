ccb79222ff8fd909100f0e3850515ca1ca6e5492
mathew_francis <mathewfvv@gmail.com>
2023-08-09 14:05:50 +0530

    first commit 1

---
+++ src/test/java/com/example/junittesting/GreetingTest.java
@@ -0,0 +1,40 @@
+package com.example.junittesting;
+
+import org.junit.jupiter.api.*;
+
+class GreetingTest {
+
+    Greeting greeting;
+    static int c = 0;
+    static int d = 0;
+    @BeforeAll
+    public static void beforeAll(){
+
+        System.out.println("beforeAll number of calls  "+ ++c);
+    }
+
+    @BeforeEach
+    void startUp(){
+        greeting = new Greeting();
+        System.out.println("startUp number of calls  "+ ++d);
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Mathew"));
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.print("after each called");
+    }
+
+    @AfterAll
+    public static void afterAllCalled(){
+        System.out.print("After all called");
+    }
+}
